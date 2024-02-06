5b11c0dc665300ca3a3de89c08c46c21e27dd84d
Josh Marsico <85882610+Origin-code@users.noreply.github.com>
2024-02-06 18:10:56 -0500

    Update hello.c with thread details

--- hello.c
+++ hello.c
@@ -5,14 +5,16 @@ int main() {
         omp_set_num_threads(8);
         #pragma omp parallel
         {
-                for (int i = 0; i < 4; i++) {
-                        int threadNum = omp_get_thread_num();
-                        int numThreads = omp_get_num_threads();
-                        //printf("Num threads\n");
-                        printf("Hello World!\n");
-                }
+                // #pragma omp for
+                        for (int i = 0; i < 4; i++) {
+                                int threadNum = omp_get_thread_num();
+                                int numThreads = omp_get_num_threads();
+                                printf("Num threads%d\n", numThreads);
+                                printf("Hello World!%d\n", threadNum);
+                        }
         }
 }
+
 
 
 
